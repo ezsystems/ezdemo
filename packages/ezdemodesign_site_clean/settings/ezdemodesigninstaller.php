23a3a7a35ff92b7c5d952ae2667d08ff255d3475
Lukasz Serwatka <l.serwatka@gazeta.pl>
2012-03-19 12:12:51 +0100

    Block defs

--- packages/ezdemodesign_site_clean/settings/ezdemodesigninstaller.php
+++ packages/ezdemodesign_site_clean/settings/ezdemodesigninstaller.php
@@ -3009,7 +3009,7 @@ class ezdemodesignInstaller extends eZSiteInstaller
                     'Subdir' => 'templates',
                     'Match' => array(
                         'type' => 'Video',
-                        'view' => 'video'
+                        'view' => 'default'
                     )
                 ),
                 'block_tag_cloud' => array(
@@ -3018,7 +3018,7 @@ class ezdemodesignInstaller extends eZSiteInstaller
                     'Subdir' => 'templates',
                     'Match' => array(
                         'type' => 'TagCloud',
-                        'view' => 'tag_cloud'
+                        'view' => 'default'
                     )
                 ),
                 'block_poll' => array(
@@ -3027,7 +3027,7 @@ class ezdemodesignInstaller extends eZSiteInstaller
                     'Subdir' => 'templates',
                     'Match' => array(
                         'type' => 'Poll',
-                        'view' => 'poll'
+                        'view' => 'default'
                     )
                 ),
                 'block_item_list' => array(
@@ -3036,7 +3036,7 @@ class ezdemodesignInstaller extends eZSiteInstaller
                     'Subdir' => 'templates',
                     'Match' => array(
                         'type' => 'ItemList',
-                        'view' => 'item_list'
+                        'view' => 'default'
                     )
                 ),
                 'block_feed_reader' => array(
@@ -3045,7 +3045,7 @@ class ezdemodesignInstaller extends eZSiteInstaller
                     'Subdir' => 'templates',
                     'Match' => array(
                         'type' => 'FeedReader',
-                        'view' => 'feed_reader'
+                        'view' => 'default'
                     ) 
                 ), 
                 'block_item_article' => array( 
