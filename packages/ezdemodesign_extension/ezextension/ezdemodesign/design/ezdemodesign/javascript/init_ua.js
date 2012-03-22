a3c6abda7e80d3db96743beff8e3dfdb7b84291f
Damien Pobel <dp@ez.no>
2012-03-22 16:06:29 +0100

    Added engine version number in html element class as well

--- packages/ezdemodesign_extension/ezextension/ezdemodesign/design/ezdemodesign/javascript/init_ua.js
+++ packages/ezdemodesign_extension/ezextension/ezdemodesign/design/ezdemodesign/javascript/init_ua.js
@@ -1,6 +1,7 @@
 YUI(YUI3_config).use('event', function (Y) {
     Y.on('domready', function () {
-        var h = Y.one('html');
+        var h = Y.one('html'),
+            ua = Y.UA;
 
         if ( h.hasClass('ie') ) {
             // conditional comments did the job
@@ -8,12 +9,12 @@ YUI(YUI3_config).use('event', function (Y) {
         }
         // Y.UA is based on the user agent string,
         // this is bad... but should be used only to fix "small" CSS issues
-        if ( Y.UA.ie ) {
+        if ( ua.ie ) {
             h.addClass('ie').addClass('ie-gt9');
-        } else if ( Y.UA.webkit ) {
-            h.addClass('webkit');
-        } else if ( Y.UA.gecko ) {
-            h.addClass('gecko');
+        } else if ( ua.webkit ) {
+            h.addClass('webkit').addClass('vers_' + (ua.webkit + '').replace('.', '_'));
+        } else if ( ua.gecko ) {
+            h.addClass('gecko').addClass('vers_' + (ua.gecko + '').replace('.', '_'));
         }
     });
 });
